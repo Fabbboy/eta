16dacf4d8ec70e5615b491f8dea3cd3d19f873bd
Fabrice <fabrice.schaub@swisscom.com>
2025-08-25 11:10:27 +0200

    use muc

--- eta/main.c
+++ eta/main.c
@@ -1,3 +1,18 @@
+#include "muc/memory/allocator.h"
+#include "muc/memory/callocator.h"
+#include "muc/memory/layout.h"
+#include "muc/option.h"
+#include <stdio.h>
+
 int main() {
-    
+  muc_allocator_t gpa = muc_callocator();
+  MUC_OPTION_NAME(ptr_t) buf = muc_allocate(gpa, MUC_LAYOUT(int));
+  if (!MUC_OPTION_METHOD(ptr_t, is_some)(buf)) {
+    printf("Allocation failed\n");
+    return 1;
+  }
+
+  *buf.value = 123;
+  printf("Allocated integer: %d\n", *buf.value);
+  muc_deallocate(gpa, buf.value, MUC_LAYOUT(int));
 }
